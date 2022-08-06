6724fe4418116117e68cfef442e8895a87da4234
Petya29 <55765677+Petya29@users.noreply.github.com>
2022-08-06 20:49:49 +0200

    Change files structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,8 @@
-import React, { useState } from 'react';
-import { useAutoAnimate } from '@formkit/auto-animate/react'
-import { Box, createTheme, CssBaseline, Divider, List, Paper, ThemeProvider } from '@mui/material';
-import { Container } from '@mui/system';
-import { TaskAddInput } from './components/TaskAddInput';
-import { ITask } from './models/ITask';
-import { TaskItem } from './components/TaskItem';
+import React from 'react';
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+import { DndList } from './pages/DndList';
 
 function App() {
-
-  const [listRef] = useAutoAnimate<HTMLDivElement>();
-
-  const [tasks, setTasks] = useState<ITask[]>([]);
-
-  const moveTasks = (currIndex: number, newIndex: number) => {
-    const taskStateCopy = [...tasks];
-    const [reorderedItem] = taskStateCopy.splice(currIndex, 1);
-    taskStateCopy.splice(newIndex, 0, reorderedItem);
-
-    setTasks(taskStateCopy);
-  }
-
-  const handleAddTask = (newTask: ITask) => {
-    setTasks(prevState => {
-      return [...prevState, newTask];
-    });
-  }
-
-  const handleEditTask = (task: ITask, index: number) => {
-    setTasks(prevState => {
-      const updState = [...prevState];
-      updState[index] = task;
-      return updState;
-    });
-  }
-
-  const hadleRemoveTask = (id: string) => {
-    setTasks(prevState => {
-      return prevState.filter(task => task.id !== id);
-    });
-  }
 
   const mode: ('dark' | 'light') = 'dark';
 
@@ -55,30 +19,7 @@ function App() {
     <div className="App">
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <Container maxWidth='md'>
-          <Box sx={{ marginTop: '20px' }}>
-            <Paper sx={{ padding: '16px' }}>
-              <TaskAddInput setTask={handleAddTask} />
-              <List sx={{ width: '100%', marginTop: '20px' }}>
-                <Box ref={listRef}>
-                  {tasks.map((task, i) => (
-                    <TaskItem
-                      index={i}
-                      task={task}
-                      isLast={(tasks.length - 1) === i ? true : false}
-                      isFirst={i === 0 ? true : false}
-                      reorder={moveTasks}
-                      editTask={handleEditTask}
-                      removeTask={hadleRemoveTask}
-                      key={task.id}
-                    />
-                  ))}
-                  <Divider />
-                </Box>
-              </List>
-            </Paper>
-          </Box>
-        </Container>
+        <DndList />
       </ThemeProvider>
     </div>
   );
