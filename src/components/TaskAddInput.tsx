a42adcde41068211b1e3cd718034535860d2a14e
Petya29 <55765677+Petya29@users.noreply.github.com>
2022-08-09 21:44:33 +0200

    Added error alert on empty task

--- src/components/TaskAddInput.tsx
+++ src/components/TaskAddInput.tsx
@@ -1,7 +1,12 @@
 import React, { FC, useState } from 'react';
-import { Box, Button, TextField } from '@mui/material';
+import { Alert, Box, Button, Slide, SlideProps, Snackbar, TextField } from '@mui/material';
 import { ITask } from '../models/ITask';
 import { v4 as uuidv4 } from 'uuid';
+
+type TransitionProps = Omit<SlideProps, 'direction'>;
+const TransitionRight = (props: TransitionProps) => {
+    return <Slide {...props} direction="left" />;
+}
 
 interface ITaskAddInput {
     setTask: (newTask: ITask) => void
@@ -10,6 +15,7 @@ interface ITaskAddInput {
 export const TaskAddInput: FC<ITaskAddInput> = ({ setTask, ...props }) => {
 
     const [newTask, setNewTask] = useState<ITask>({ id: '', title: '', subTasks: [] } as ITask);
+    const [isSnackbarOpen, setIsSnackbarOpen] = useState<boolean>(false);
 
     const handleChangeTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
         setNewTask({
@@ -23,12 +29,25 @@ export const TaskAddInput: FC<ITaskAddInput> = ({ setTask, ...props }) => {
     }
 
     const addNewTask = () => {
-        setTask({ ...newTask, id: uuidv4() });
+        if (newTask.title.trim() === '') {
+            setIsSnackbarOpen(true);
+            return;
+        }
+
+        setTask({ ...newTask, title: newTask.title.trim(), id: uuidv4() });
         setNewTask({
             id: '',
             title: '',
             subTasks: []
         });
+    }
+
+    const handleSnackbarClose = (event: React.SyntheticEvent | Event, reason?: string) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setIsSnackbarOpen(false);
     }
 
     return (
@@ -52,6 +71,18 @@ export const TaskAddInput: FC<ITaskAddInput> = ({ setTask, ...props }) => {
             >
                 {'Add'}
             </Button>
+            <Snackbar
+                anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+                open={isSnackbarOpen}
+                autoHideDuration={2000}
+                onClose={handleSnackbarClose}
+                TransitionComponent={TransitionRight}
+                sx={{ display: isSnackbarOpen ? 'flex' : 'none' }}
+            >
+                <Alert onClose={handleSnackbarClose} severity='error' sx={{ width: '100%' }}>
+                    Task title cannot be empty!
+                </Alert>
+            </Snackbar>
         </Box>
     )
 }
